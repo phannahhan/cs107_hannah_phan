45c8cfb3622b0a6309edd226590a25630b389b1a
Hannah Phan <hannahphan@dhcp-10-250-188-104.harvard.edu>
2021-09-08 21:33:06 -0400

    P3.sh without part E, still working on it

--- homework/HW1/HW1-final/P3.sh
+++ homework/HW1/HW1-final/P3.sh
@@ -1,3 +1,5 @@
-grep -c "^[0-9]" apollo13.txt > apollo_out.txt
+grep -c "[0-9]" apollo13.txt > apollo_out.txt
 grep --help |& fgrep -e --count
-ls -d *.py* | wc -l 
+ls -d *.py* | wc -l
+ls -l | grep "^-rwxrwx--x" DOUBLE CHECK IF CONDITION IS MET
+ 
