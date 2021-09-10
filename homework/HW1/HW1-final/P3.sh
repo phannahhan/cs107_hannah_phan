bc53df49d1369a5bebe73f9470c11ea7ccd5d3d4
Hannah Phan <hannahphan@dhcp-10-250-204-65.harvard.edu>
2021-09-10 17:10:55 -0400

    Changed permissions part E

--- homework/HW1/HW1-final/P3.sh
+++ homework/HW1/HW1-final/P3.sh
@@ -1,6 +1,6 @@
 grep -c "[0-9]" apollo13.txt > apollo_out.txt
 grep --help |& fgrep -e --count
 ls -d *.py* | wc -l
-find . -type f | ls -l | grep "^-rwxrwx--x"
-find . -maxdepth 1 | ls -l | grep "^-rwxrwx--x"
+find . -type f | ls -l | grep "^-......--."
+find . -maxdepth 1 | ls -l | grep "^-......--."
  
