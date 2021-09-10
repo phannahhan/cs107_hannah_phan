1ed6a3ae415e06c786cc9caa90ed1e06bd8b836a
Hannah Phan <hannahphan@dhcp-10-250-188-104.harvard.edu>
2021-09-09 20:18:45 -0400

    Completed P3.sh

--- homework/HW1/HW1-final/P3.sh
+++ homework/HW1/HW1-final/P3.sh
@@ -1,5 +1,6 @@
 grep -c "[0-9]" apollo13.txt > apollo_out.txt
 grep --help |& fgrep -e --count
 ls -d *.py* | wc -l
-ls -l | grep "^-rwxrwx--x" DOUBLE CHECK IF CONDITION IS MET
+find . -type f | ls -l | grep "^-rwxrwx--x"
+find . -maxdepth 1 | ls -l | grep "^-rwxrwx--x"
  
