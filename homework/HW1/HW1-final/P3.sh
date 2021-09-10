ab8efa612ab32b9fb83da76309cf238031601f3f
Hannah Phan <hannahphan@dhcp-10-250-204-65.harvard.edu>
2021-09-10 17:35:29 -0400

    Fixed part E

--- homework/HW1/HW1-final/P3.sh
+++ homework/HW1/HW1-final/P3.sh
@@ -1,6 +1,6 @@
 grep -c "[0-9]" apollo13.txt > apollo_out.txt
 grep --help |& fgrep -e --count
 ls -d *.py* | wc -l
-find . -type f | ls -l | grep "^-......--."
-find . -maxdepth 1 | ls -l | grep "^-......--."
+ls -al | grep "\-......\-\-."
+ls -al | grep "[d-]......\-\-."
  
